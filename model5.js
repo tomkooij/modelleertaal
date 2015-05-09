9d05173e03379413fa4fb7985f827d546c55ae77
tomkooij <tomkooij@tomkooij.nl>
2015-05-09 15:02:26 +0200

    model 5 in javascript... 200x times faster than interpreter and/or Coach6

---
+++ model5.js
@@ -0,0 +1,43 @@
+/*
+SysNat deel 5 H5 model 5 Optrekken van een auto
+modelregels
+
+Pure javascript for speed comparision
+
+*/
+
+function model() {
+
+    /* startwaarden */
+    const Fmotor = 500;
+    const m = 800;
+    const dt = 1e-5;
+    var v = 0;
+    var s = 0;
+    var t = 0;
+
+    var Fres;
+    var a;
+    var dv, ds;
+
+    for (var i = 0; i < 1e6; i++) {
+
+        /* model */
+        Fres = Fmotor;
+        a = Fres/m;
+        dv = a * dt;
+        v = v + dv;
+        ds = v * dt;
+        s = s + ds
+        if (1) { t = t + dt; }
+    }
+
+    console.log('t=',t);
+    console.log('s=',s);
+};
+
+
+var t1 = Date.now();
+model();
+var t2 = Date.now();
+console.log("Time: " + (t2 - t1) + "ms");
