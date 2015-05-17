264e6a371c0d3dce45212302ebe97edfe49500f1
tomkooij <tomkooij@tomkooij.nl>
2015-05-17 21:11:17 +0200

    unittest for parseBogusXMLString in model.js

--- test/model_tests.js
+++ test/model_tests.js
@@ -3,7 +3,16 @@ var assert = require("assert"); // node.js core module
 var modelclass = require("../model.js").Model;
 var testmodel = new modelclass();
 
+var bogusModel = '<modelleertaal>\n\r' +
+                        '<modelregels>\nt=t+dt\np=5\n</modelregels>\n\r' +
+                        '<startwaarden>\nParseMe!\n</startwaarden>\n';
+
 describe('model.js - Read model from XML', function(){
+
+    it('parseBogusXMLString parses test string', function() {
+        testmodel.parseBogusXMLString(bogusModel);
+        assert.equal(testmodel.startwaarden,'/* startwaarden */\nParseMe!\n');
+    })
 
     it('readBogusXMLFile reads and parses testmodel.xml', function() {
         testmodel.readBogusXMLFile('test/testmodel/testmodel.xml');
