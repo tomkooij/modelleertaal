f3297cfb1b574adab36e4f9b4dd2561d51eb4db1
Tom Kooij <tomkooij@tomkooij.nl>
2017-10-17 12:01:10 +0200

    Remove xml dependency

--- model.js
+++ model.js
@@ -38,7 +38,6 @@
 
 //jshint es3:true
 
-var xml = require('node-xml-lite');
 var fs = require('fs');
 
 function Model() {
@@ -46,40 +45,6 @@ function Model() {
     this.startwaarden = '';
 }
 
-Model.prototype.readXMLFile = function(filename) {
-
-    var xmlJSON = xml.parseFileSync(filename);
-    this.parseXML(xmlJSON);
-};
-
-Model.prototype.readXMLString = function(xmlString) {
-
-    var xmlJSON = xml.parseString(xmlString);
-    this.parseXML(xmlJSON);
-};
-
-
-Model.prototype.parseXML = function(xmlJSON) {
-
-    if (xmlJSON.name == 'modelleertaal') {
-
-        for (var i = 0; i < xmlJSON.childs.length; i++) {
-
-            switch(xmlJSON.childs[i].name){
-                case 'startwaarden':  {
-                    this.startwaarden = xmlJSON.childs[i].childs[0];
-                    break;
-                }
-                case 'modelregels':  {
-                    this.modelregels = xmlJSON.childs[i].childs[0];
-                    break;
-                }
-                default:
-                        throw new Error('Unable to handle xml item: ', xmlJSON.childs[i]);
-            }
-        }
-    }
-};
 
 Model.prototype.readBogusXMLFile = function(filename) {
     // This read a "bogus" XML file that still includes < instead of &lt;
