e37cf3f4e620ff5eb16772b4b7e1a4cee1409e4f
Tom Kooij <tomkooij@tomkooij.nl>
2017-10-18 13:20:20 +0200

    Model index in models.js

---
+++ modellen/models.js
@@ -0,0 +1,7 @@
+// definieer hier de modellen
+
+var model_index = [
+    {url:'modellen/examenvwo2005.xml', title: 'Examen vwo 2005-I Champignon'},
+    {url:'modellen/examenvwo2016.xml', title: 'Examen vwo 2016-I Ruimtelift'},
+    {url:'modellen/satelliet.xml', title: 'Opgave 17: Satelliet'},
+  ];
