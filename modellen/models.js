d193677e7d4de343c10865a6869e01cebf41748a
Tom Kooij <tomkooij@tomkooij.nl>
2017-10-21 12:58:23 +0200

    Add examen vwo 2017-II X-Stream
    [ci skip]

--- modellen/models.js
+++ modellen/models.js
@@ -6,6 +6,7 @@ var model_index = [
     {url:'modellen/examenvwo2015.xml', title: 'Examen vwo 2015-I Sprong bij Volleybal'},
     {url:'modellen/examenvwo2016.xml', title: 'Examen vwo 2016-I Ruimtelift'},
     {url:'modellen/examenvwo2016-2.xml', title: 'Examen vwo 2016-II Jupiter Fly-by'},
+    {url:'modellen/examenvwo2017-2.xml', title: 'Examen vwo 2017-II X-Stream'},
     {url:'modellen/parachutesprong.xml', title: 'Opgave 9: Parachutesprong'},
     {url:'modellen/satelliet.xml', title: 'Opgave 17: Satelliet'},
   ];
