f79b67554de51c4523c3d81bf4cf1b64d6279d4a
Tom Kooij <tomkooij@tomkooij.nl>
2017-10-21 09:33:51 +0200

    Add examen vwo 2015 and 2016-II

--- modellen/models.js
+++ modellen/models.js
@@ -2,7 +2,9 @@
 
 var model_index = [
     {url:'modellen/examenvwo2005.xml', title: 'Examen vwo 2005-I Champignon'},
-    {url:'modellen/examenvwo2006.xml', title: 'Examen vwo 2006-I Sojoez'},
+    {url:'modellen/examenvwo2006.xml', title: 'Examen vwo 2006-II Sojoez'},
+    {url:'modellen/examenvwo2015.xml', title: 'Examen vwo 2015-I Sprong bij Volleybal'},
     {url:'modellen/examenvwo2016.xml', title: 'Examen vwo 2016-I Ruimtelift'},
+    {url:'modellen/examenvwo2016-2.xml', title: 'Examen vwo 2016-II Jupiter Fly-by'},
     {url:'modellen/satelliet.xml', title: 'Opgave 17: Satelliet'},
   ];
