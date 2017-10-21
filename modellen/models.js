1e5ddc305d56aa6004c338cb3a43795e6d63f206
Tom Kooij <tomkooij@tomkooij.nl>
2017-10-21 09:06:01 +0200

    Add examen vwo 2006-I Sojoez

--- modellen/models.js
+++ modellen/models.js
@@ -2,6 +2,7 @@
 
 var model_index = [
     {url:'modellen/examenvwo2005.xml', title: 'Examen vwo 2005-I Champignon'},
+    {url:'modellen/examenvwo2006.xml', title: 'Examen vwo 2006-I Sojoez'},
     {url:'modellen/examenvwo2016.xml', title: 'Examen vwo 2016-I Ruimtelift'},
     {url:'modellen/satelliet.xml', title: 'Opgave 17: Satelliet'},
   ];
