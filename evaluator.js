a032eeaf0644ac60c536a88527ecfdffcb00fee1
Tom Kooij <tomkooij@tomkooij.nl>
2019-10-01 12:17:36 +0200

    Create (y,x) diagram for 2D models with y and x
    Also (h,x) diagram
    
    fixes gh-26

--- evaluator.js
+++ evaluator.js
@@ -45,6 +45,55 @@ parser.performAction = function anonymous(yytext,yyleng,yylineno,yy,yystate,$$,_
     return ret;
 };
 
+// Array.prototype.includes polyfill (ECMAScript 7)
+// https://tc39.github.io/ecma262/#sec-array.prototype.includes
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(searchElement, fromIndex) {
+
+      // 1. Let O be ? ToObject(this value).
+      if (this === null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        // c. Increase k by 1.
+        // NOTE: === provides the correct "SameValueZero" comparison needed here.
+        if (o[k] === searchElement) {
+          return true;
+        }
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
 
 /*
  Class namespace
@@ -140,7 +189,17 @@ Namespace.prototype.sortVarNames = function () {
     // now sorts on variable NAME. Should identify stock variables in AST.
 
     // names of "special"variable names to sort, sort if found in order given
-    var nameList = ['t', 's', 'x', 'y', 'h', 'v', 'vx', 'vy'];
+    var nameList;
+    if (this.varNames.includes('y') & this.varNames.includes('x')) {
+      // try to plot y,x diagram
+      nameList = ['x', 'y', 't', 's', 'h', 'v', 'vx', 'vy'];
+    } else if (this.varNames.includes('h') & this.varNames.includes('x')) {
+      // try to plot y,x diagram
+      nameList = ['x', 'h', 't', 's', 'y', 'v', 'vx', 'vy'];
+    } else {
+      // try to plot t,x diagram
+      nameList = ['t', 's', 'x', 'y', 'h', 'v', 'vx', 'vy'];
+    }
     var nextVariableIndex = 0 ; // place to swap next "special"variable with
 
     /*  nextVariableIndex = 0
