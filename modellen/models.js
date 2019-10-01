17bb5c405157d507d8701339dbacde628c440cf9
Tom Kooij <tomkooij@tomkooij.nl>
2019-10-01 10:40:59 +0200

    Add 2019-2 Pariser Kanone

--- modellen/models.js
+++ modellen/models.js
@@ -11,4 +11,5 @@ var model_index = [
     {url:'modellen/examenvwo2017.xml', title: 'Examen vwo 2017-I Cessna'},
     {url:'modellen/examenvwo2017-2.xml', title: 'Examen vwo 2017-II X-stream'},
     {url:'modellen/examenvwo2018.xml', title: 'Examen vwo 2018-I Uitrijden van een auto'},
+    {url:'modellen/examenvwo2019-2.xml', title: 'Examen vwo 2019-II Pariser Kanone'},
 ];
