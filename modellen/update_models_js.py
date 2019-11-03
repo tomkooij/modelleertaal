213d31468940b5286ec83b3ae1b946f7854a1e18
Tom Kooij <tomkooij@tomkooij.nl>
2019-11-03 10:58:45 +0100

    Update_models_js: change only new files
    Change the filename of new discovered (added) files only.
    Try to switch titel/path when titel/path are switched in older models.js
    Better assertions

--- modellen/update_models_js.py
+++ modellen/update_models_js.py
@@ -6,6 +6,24 @@ import shutil
 FOLDER_PREFIX = 'modellen/'
 MODELS_JS = 'models.js'
 
+def parse_line(line):
+    """ parse model.js line-by-line using voodoo-regexp:
+
+    '{title: "Bungeejumper", url: "modellen/Bungeejumper.xml"},\n'
+        =>  ["Bungeejumper", "modellen/Bungeejumper.xml"]
+
+    [\"|']       --> match " or '
+    (.[^'|\"]*)  --> group: match anything except " or '
+    [\"|']       --> match " or '
+
+    """
+    return re.findall("[\"|'](.[^'|\"]*)[\"|']", line)
+assert parse_line('{title: "Bungeejumper", url: "modellen/Bungeejumper.xml"},\n') == \
+                  ["Bungeejumper", "modellen/Bungeejumper.xml"]
+#assert parse_line("{title: 'Bungeejumper', url: 'modellen/Bungeejumper.xml'},\n") == \
+#                  ["Bungeejumper", "modellen/Bungeejumper.xml"]
+
+
 def maak_eerste_letter_hoofdletter(s):
     """maak van de eerste letter een hoofdletter"""
     return s[0].upper()+s[1:]
@@ -33,52 +51,65 @@ def write_models_js(modellen, outfile_fn=MODELS_JS):
             pass
         outfile.write('];')
 
+
 def read_models_js(models_js=MODELS_JS):
     modellen = {}
     bestanden_in_models_js = []
 
     with open(models_js) as f:
         for line in f.readlines():
-            match = re.findall("[\"|'](.[^'|\"]*)[\"|']", line)
+            match = parse_line(line)
             if match:
                 titel, path = match
-                _, fn = path.split('/')
+                if titel.startswith(FOLDER_PREFIX) and titel.endswith('.xml'):
+                    # draai path/titel om
+                    titel, path = path, titel
+                prefix, fn = path.split('/')
+                assert prefix + '/' == FOLDER_PREFIX, f"Error prefix: != FOLDER_PREFIX"
                 modellen[path] = titel
                 bestanden_in_models_js.append(fn)
 
-
     return modellen, bestanden_in_models_js
 
-def main():
 
-    modellen, bestanden_in_models_js = read_models_js()
+def add_new_files_to_index(bestanden_in_folder, bestanden_in_models_js, modellen):
+    """add new files to models.js, rename files with spaces"""
 
-    bestanden_in_folder = glob.glob('*.xml')
-
-    # skip missing/removed xml files (delete from models.js)
-    for fn in bestanden_in_models_js:
-        if fn not in bestanden_in_folder:
-            print(f'file not found: {fn}: skipping.')
-            del modellen[fn_to_path(fn)]
-
-    # rename files with spaces, add new files to models.js
     for fn in bestanden_in_folder:
-        if " " in fn:
-            fn_new = fn.replace(' ', '_')
-            fn_new = maak_eerste_letter_hoofdletter(fn_new)
-            print(f'rename: {fn} -> {fn_new}')
-            shutil.move(fn, fn_new)
-            fn = fn_new
         if fn not in bestanden_in_models_js:
             print(f'new file: {fn}')
-            assert fn[-4:] == '.xml', 'ABORT: filename != *.xml'
-            path = fn_to_path(fn)
+            assert fn.endswith('.xml'), 'ABORT: filename != *.xml'
 
+            if " " in fn:
+                fn_new = fn.replace(' ', '_')
+                fn_new = maak_eerste_letter_hoofdletter(fn_new)
+                print(f'rename: {fn} -> {fn_new}')
+                shutil.move(fn, fn_new)
+                fn = fn_new
+
+            path = fn_to_path(fn)
             # default titel = bestandsnaam zonder .xml
             titel = maak_eerste_letter_hoofdletter(fn.replace('_', ' '))
             titel = titel[:-4]  # remove '.xml'
             modellen[path] = titel
 
+
+def remove_missing_files_from_index(bestanden_in_models_js, bestanden_in_folder, modellen):
+    """skip missing/removed xml files (remove (don't add) to models.js)"""
+
+    for fn in bestanden_in_models_js:
+        if fn not in bestanden_in_folder:
+            print(f'file not found: {fn}: skipping.')
+            del modellen[fn_to_path(fn)]
+
+
+def main():
+
+    modellen, bestanden_in_models_js = read_models_js()
+    bestanden_in_folder = glob.glob('*.xml')
+
+    remove_missing_files_from_index(bestanden_in_models_js, bestanden_in_folder, modellen)
+    add_new_files_to_index(bestanden_in_folder, bestanden_in_models_js, modellen)
     write_models_js(modellen)
 
 
