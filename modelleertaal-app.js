4d6e65cb2c6d28baea47ac54422639fcfa8745bc
Tom Kooij <tomkooij@tomkooij.nl>
2019-11-07 18:00:21 +0100

    Add different axes to pgfplots

--- modelleertaal-app.js
+++ modelleertaal-app.js
@@ -668,8 +668,9 @@ ModelleertaalApp.prototype.create_pgfplot_header = function() {
 		return "%x and y scale set to 10cmx10cm grid. Adjust to fit!\n" +
 		 "% x = ["+x_min+" .. "+arrayMax(x)+"]\n"+
 		 "% y = ["+y_min+" .. "+arrayMax(y)+"]\n"+
-		 "% this only works for graphs starting a (0,0)\n"+
+		 "% this only works for graphs starting at (0,0)\n"+
 		 "\\begin{axis}[x=1cm\/"+x_scale+", y=1cm\/"+y_scale+",\n"+
+     "%axis lines*=center,"+
 		 "enlargelimits=false, tick align=outside,\n "+
 		 "xlabel={$"+x_var+"$ [\\si{"+x_unit+"}]},\n"+
 		 "ylabel={$"+y_var+"$ [\\si{"+y_unit+"}]},\n"+
